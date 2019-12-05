aaa211b795b3383bd9f323be363a8b667a322f62
Mattgh91 <Mattgh9152@gmail.com>
2019-12-05 10:05:03 +0000

    Change .gitignore + commit logs

---
+++ src/components/logs/LogItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LogItem = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+LogItem.propTypes = {
+    
+};
+
+export default LogItem;
