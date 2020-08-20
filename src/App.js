a479342604b870895e0690652ccc600f54ceb42d
Mattgh91 <Mattgh9152@gmail.com>
2020-08-20 23:00:48 +0100

    Bring in Redux - set up store & provider. Logs reducer, actions & types. Connect redux to logs component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -9,6 +9,8 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
@@ -16,17 +18,19 @@ const App = () => {
     useEffect(() => M.AutoInit());
 
     return (
-        <>
-            <SearchBar/>
-            <div>
-                <Logs/>
-            </div>
-            <AddBtn />
-            <AddLogModal />
-            <EditLogModal />
-            <AddTechModal />
-            <TechListModal />
-        </>
+        <Provider store={store} >
+            <>
+                <SearchBar/>
+                <div>
+                    <Logs/>
+                </div>
+                <AddBtn />
+                <AddLogModal />
+                <EditLogModal />
+                <AddTechModal />
+                <TechListModal />
+            </>
+        </Provider>
     );
 };
 
